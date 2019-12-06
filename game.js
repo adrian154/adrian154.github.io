e5b167cfb1702eea4827db63f6868d26d461dbe4
adrian <94568a@gmail.com>
2019-12-05 17:32:44 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 
-const GRAV_CONST = 1.2;
+const GRAV_CONST = 2.0;
 const DT = 1 / 60;
 const RADIUS = 12;
 const PARTICLE_RADIUS = 5;
