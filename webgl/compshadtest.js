4c52e05fbf1db14f055ec88b70bbffc286fd762f
adrian154 <94568a@gmail.com>
2020-04-02 16:41:47 -0700

    shading test

--- webgl/compshadtest.js
+++ webgl/compshadtest.js
@@ -6,6 +6,37 @@ const shaderSource = `#version 310 es
 layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 layout (rgba8, binding = 0) writeonly uniform highp image2D outputTexture;
 
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+vec3 pointOnRay(Ray ray, float t) {
+    return ray.origin + ray.direction * t;
+}
+
+vec3 intersect(Ray ray) {
+
+    /* Unit sphere at 0,0 */
+    vec3 sphereCenter = vec3(0.0, 0.0, 3.0);
+    float radius = 1.0;
+
+    vec3 center = sphereCenter - ray.origin;
+
+    float b = -2.0 * dot(ray.direction, center);
+    float c = dot(center, center) - radius * radius;
+
+    float disc = b * b - 4.0 * c;
+    if(disc >= 0.0) {
+        vec3 normal = normalize(pointOnRay(ray, min((-b-disc)/2.0, (-b+disc)/2.0)) - sphereCenter);
+        float shade = dot(normal, vec3(0.0, 0.0, 0.0) - ray.direction);
+        return vec3(shade, shade, shade);
+    }
+
+    return vec3(0.0, 0.0, 0.0);
+
+} 
+
 void main() {
     
     /* Exact x-y of output */
@@ -13,7 +44,12 @@ void main() {
     ivec2 imageSize = ivec2(gl_NumWorkGroups.xy * gl_WorkGroupSize.xy);
     vec2 uv = vec2(storePos) / vec2(imageSize);
 
-    vec4 color = vec4(uv.x, 1.0, uv.y, 1.0);
+    vec4 color;
+    Ray ray;
+    ray.origin = vec3(0.0, 0.0, 0.0);
+    ray.direction = normalize(vec3(uv.x - 0.5, uv.y - 0.5, 1.0));
+
+    color = vec4(intersect(ray), 1.0);
     imageStore(outputTexture, storePos, color);
     
 }
