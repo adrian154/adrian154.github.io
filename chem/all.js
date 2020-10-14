ccaf5785113ed3dcf605f17d3efa34d7b1623f97
adrian154 <94568a@gmail.com>
2020-10-13 22:05:21 -0700

    a

--- chem/all.js
+++ chem/all.js
@@ -7,8 +7,7 @@ const quantumNumbersToAtomic = function(inN, inL, m, s) {
     // this will never return for bogus inputs, make sure they are checked!
     let nmaster = 1, lmaster = 0, electrons = 0;
 
-    let i = 0;
-    while(i < 10) {
+    while(true) {
         
         // do the staircase descend thing
         let n = nmaster, l = lmaster;
@@ -43,5 +42,55 @@ const configToAtomic = function(configString) {
     }
 
     return electrons;
+
+};
+
+// see quantum to atomic - same code
+const atomicToQuantum = function(atomic) {
+
+    let nmaster = 1, lmaster = 0;
+
+    let electronsRemaining = atomic;
+    while(true) {
+        let n = nmaster, l = lmaster;
+        do {
+            if(2 * (l * 2 + 1) < electronsRemaining) {
+                console.log(`Full-filling n=${n}, l=${l}`);
+                electronsRemaining -= 2 * (l * 2 + 1);
+            } else {
+
+                // hunds rule but in reverse
+                // somehow even more annoying
+                let s;
+                if(electronsRemaining > l * 2 + 1) {
+                    s = 0;
+                    electronsRemaining -= l * 2 + 1;    
+                } else {
+                    s = 1;
+                }
+
+                return {
+                    n: n,
+                    l: l,
+                    m: electronsRemaining - l - 1,
+                    s: s
+                }
+
+            }
+            l--;
+            n++;
+        } while(l >= 0);
+        if(lmaster + 1 == nmaster)
+            nmaster++;
+        else
+            lmaster++;
+    }
+
+
+};
+
+const atomicToConfig = function(atomic) {
+
+
 
 };
