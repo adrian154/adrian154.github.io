d9f816470b8730f4a2cb68ac06ef8c7e4eb08fe1
adrian154 <94568a@gmail.com>
2020-10-22 13:53:24 -0700

    functioning racyast

--- games/raycast/raycast.js
+++ games/raycast/raycast.js
@@ -6,11 +6,12 @@ const radToDeg = (radians) => radians * 180 / Math.PI;
 const degToRad = (degrees) => degrees * Math.PI / 180;
 const FOVToFocal = (degrees) => canvas.width / (2 * Math.tan(degToRad(degrees) / 2));
 const floor = (x) => x < 0 ? Math.ceil(x) : Math.floor(x);
-//const nextGridPos = (x, dir) => Math.floor(((x + dir * tileSize) + tileSize / 2) / tileSize) * tileSize;
-const nextGridPos = (x, dir) => (floor(x / tileSize) + dir) * tileSize - dir * (tileSize / 2);
+//const nextGridPos = (x, dir) => (dir < 0 ? Math.floor : Math.ceil)((x + 0.001) / tileSize) * tileSize;
+const nextGridPos = (x, dir) => (Math.floor(x / tileSize) + (dir > 0 ? 1 : -0.0001)) * tileSize;
 
 // Constants
-const tileSize = 10;
+const tileSize = 50;
+const minimapScale = 1;
 
 // Map
 const MAP_WIDTH = 5;
@@ -18,9 +19,9 @@ const MAP_HEIGHT = 5;
 const map = [
     [1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1]
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0]
 ];
 
 // Initialize globals
@@ -28,7 +29,7 @@ const map = [
 let posX = 30;
 let posY = 30;
 let viewAngle = Math.random() * 2 * Math.PI;
-let FOV = 120;
+let FOV = 60;
 let focalLength = FOVToFocal(FOV);
 const controls = {
     forward: false,
@@ -40,44 +41,55 @@ const controls = {
 };
 
 // Trace the ray
-const traceRay = function(start, ray) {
+const traceRay = function(start, ray, debug) {
 
-    let tileX = Math.floor((start[0] - tileSize / 2) / tileSize);
-    let tileY = Math.floor((start[1] - tileSize / 2) / tileSize);
+    let tileX = Math.floor(start[0] / tileSize);
+    let tileY = Math.floor(start[1] / tileSize);
     let cur = [...start]; // wasteful shallow copy but whatever
 
     let yPerX = ray[1] / ray[0];
     let xPerY = ray[0] / ray[1];
 
+    if(debug)
+        console.log(`start=(${tileX}, ${tileY})`);
+
     while(true) {
 
-        //console.log(`Start: cur=(${cur[0]}, ${cur[1]}), tile=(${tileX}, ${tileY})`);
+        if(debug) {
+            console.log(`cur=(${cur[0]}, ${cur[1]}), tile=(${tileX}, ${tileY})`);
+        }
 
-        let nextHorizDY = nextGridPos(cur[1], Math.sign(ray[1])) - cur[1];
-        let nextHorizDX = nextHorizDY * xPerY;
-        let nextHorizDistSq = nextHorizDX * nextHorizDX + nextHorizDY * nextHorizDY;
+        let nextHorizY = nextGridPos(cur[1], Math.sign(ray[1]));
+        let nextHorizX = (nextHorizY - cur[1]) * xPerY + cur[0];
 
-        let nextVertDX = nextGridPos(cur[0], Math.sign(ray[0])) - cur[0];
-        let nextVertDY = nextVertDX * yPerX;
-        let nextVertDistSq = nextVertDX * nextVertDX + nextVertDY * nextVertDY;
+        let nextVertX = nextGridPos(cur[0], Math.sign(ray[0]));
+        let nextVertY = (nextVertX - cur[0]) * yPerX + cur[1];
+        if(debug) console.log(cur[0], Math.sign(ray[0]));
 
-        //console.log(`Next horizontal: (${cur[0] + nextHorizDX}, ${cur[1] + nextHorizDY})`);
-        //console.log(`Next vertical: (${cur[1] + nextVertDX}, ${cur[1] + nextVertDY})`);
+        let nextHorizDistSq = (nextHorizX - cur[0]) * (nextHorizX - cur[0]) + (nextHorizY - cur[1]) * (nextHorizY - cur[1]); 
+        let nextVertDistSq = (nextVertX - cur[0]) * (nextVertX - cur[0]) + (nextVertY - cur[1]) * (nextVertY - cur[1]);
 
-        if(isFinite(nextHorizDistSq) ? nextHorizDistSq : Infinity < isFinite(nextVertDistSq) ? nextVertDistSq : Infinity) {
-            cur[0] += nextHorizDX;
-            cur[1] += nextHorizDY;
+        //console.log(`horizontal=(${nextHorizX}, ${nextHorizY}), sqDist=${nextHorizDistSq}`);
+        //console.log(`vertical=(${nextVertX}, ${nextVertY}), sqDist=${nextVertDistSq}`);
+
+        if(isFinite(nextHorizDistSq) && (!isFinite(nextVertDistSq) || nextHorizDistSq < nextVertDistSq)) {
+            if(debug) console.log("going horizontal" + nextHorizDistSq);
+            cur[0] = nextHorizX;
+            cur[1] = nextHorizY;
             tileY += Math.sign(ray[1]);
         } else {
-            cur[0] += nextVertDX;
-            cur[1] += nextVertDY;
+            if(debug) console.log("going vertical, d=" + nextVertDistSq);
+            cur[0] = nextVertX;
+            cur[1] = nextVertY;
             tileX += Math.sign(ray[0]);
         }
 
-        //console.log(`Now: cur=(${cur[0]}, ${cur[1]}), tile=(${tileX}, ${tileY})`);
         if(tileX < 0 || tileY < 0 || tileX >= MAP_WIDTH || tileY >= MAP_HEIGHT) break;
 
         if(map[tileX][tileY]) {
+            if(debug) {
+                console.log(`final=(${tileX}, ${tileY}), position=(${cur[0]}, ${cur[1]})`)
+            }
             let dx = cur[0] - start[0];
             let dy = cur[1] - start[1];
             return Math.sqrt(dx * dx + dy * dy);
@@ -97,26 +109,23 @@ const draw = function() {
     ctx.fillStyle = "#ffffff";
 
     let debugDist;
-
     for(let screenX = 0; screenX < canvas.width; screenX++) {
 
         // interpolate angle
         let angle = degToRad((screenX / canvas.width) * FOV - FOV / 2) + viewAngle;
-        
+
         // ray
         let dx = Math.cos(angle);
         let dy = Math.sin(angle);
         
         let dist = traceRay([posX, posY], [dx, dy]);
+        if(screenX == Math.floor(canvas.width / 2)) debugDist = dist;
         if(isFinite(dist)) {
-            //let fprime = focalLength / Math.cos(viewAngle - angle);
-            let width = tileSize * focalLength / dist;
-            let color = 9550 / dist;
+            let width = tileSize * focalLength / dist/ 5;
+            let color = 2500 / dist;
             ctx.fillStyle = `rgb(${color},${color},${color})`;
             ctx.fillRect(screenX, (canvas.height - width) / 2, 1, width);
         }
-
-        if(screenX == Math.floor(canvas.width / 2)) debugDist = dist;
 
     }
 
@@ -125,7 +134,7 @@ const draw = function() {
         for(let y = 0; y < map[x].length; y++) {
 
             if(map[x][y])
-                ctx.fillRect(x * tileSize * 3, y * tileSize * 3, tileSize * 3, tileSize * 3);
+                ctx.fillRect(x * tileSize * minimapScale, y * tileSize * minimapScale, tileSize * minimapScale, tileSize * minimapScale);
 
         }
     }
@@ -136,14 +145,14 @@ const draw = function() {
     ctx.fillText(viewAngle.toFixed(2), 2, 48);
 
     ctx.beginPath();
-    ctx.arc(posX * 3, posY * 3, 3, 0, 2 * Math.PI);
+    ctx.arc(posX * minimapScale, posY * minimapScale, 3, 0, 2 * Math.PI);
     ctx.closePath();
     ctx.fill();
 
     ctx.strokeStyle = "#00ff00";
     ctx.beginPath();
-    ctx.moveTo(posX*3, posY*3);
-    ctx.lineTo(posX*3 + Math.cos(viewAngle)*debugDist*3, posY*3 + Math.sin(viewAngle)*debugDist*3/5);
+    ctx.moveTo(posX * minimapScale, posY * minimapScale);
+    ctx.lineTo(posX * minimapScale + Math.cos(viewAngle) * debugDist * minimapScale, posY * minimapScale + Math.sin(viewAngle) * debugDist * minimapScale);
     ctx.closePath();
     ctx.stroke();
 
